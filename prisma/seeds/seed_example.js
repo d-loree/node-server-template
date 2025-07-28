7852fba44ac4ffdf6b712b38560f698681167cad
Dylan Loree <dylan.loree@gmail.com>
2025-07-27 22:55:51 -0400

    f

--- prisma/seeds/seed_example.js
+++ prisma/seeds/seed_example.js
@@ -1,10 +1,11 @@
-export default async function seedUsers(prisma) {
-  await prisma.user.deleteMany();
+// Seed file example for a user table
+// export default async function seedUsers(prisma) {
+//   await prisma.user.deleteMany();
 
-  await prisma.user.createMany({
-    data: [
-      { name: 'Alice', email: 'alice@example.com' },
-      { name: 'Bob', email: 'bob@example.com' },
-    ],
-  });
-}
+//   await prisma.user.createMany({
+//     data: [
+//       { name: 'Alice', email: 'alice@example.com' },
+//       { name: 'Bob', email: 'bob@example.com' },
+//     ],
+//   });
+// }
