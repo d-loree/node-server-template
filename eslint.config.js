08559a0a2be94d354a60705084b85035c5389613
Dylan Loree <dylan.loree@gmail.com>
2025-07-27 22:52:27 -0400

    f

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
@@ -12,6 +13,7 @@ export default [
       ecmaVersion: 2021,
       sourceType: 'module',
       globals: {
+        ...globals.node,
         require: true,
         module: true,
         process: true,
@@ -30,6 +32,14 @@ export default [
     rules: {
       'prettier/prettier': 'error',
       'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['**/__tests__/**/*.test.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
     },
   },
 ];
